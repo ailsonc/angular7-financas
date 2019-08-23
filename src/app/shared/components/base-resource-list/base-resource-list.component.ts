e5b25e2ae4cce17d2caa9d197bf45d1b8f5179dd
ailson <ailsonlcosta@gmail.com>
2019-08-23 17:29:52 -0300

    adding BaseResourceListComponent

--- src/app/shared/components/base-resource-list/base-resource-list.component.ts
+++ src/app/shared/components/base-resource-list/base-resource-list.component.ts
@@ -3,27 +3,27 @@ import { BaseResourceModel } from '../../models/base-resource.model';
 import { BaseResourceService } from '../../services/base-resource-service';
 
 export abstract class BaseResourceListComponent<T extends BaseResourceModel> implements OnInit {
-  resources: T[] = [];
+    resources: T[] = [];
 
-  constructor(
-    protected resourceService: BaseResourceService<T>,
-) {}
+    constructor(
+        protected resourceService: BaseResourceService<T>,
+    ) { }
 
-  ngOnInit() {
-    this.resourceService.getAll().subscribe(
-        resources => this.resources = resources,
-      error => alert('Erro ao carregar a lista')
-    );
-  }
-
-  deleteResource(resource: T) {
-    const mustDelete = confirm('Deseja excluir esse item');
-    if (mustDelete) {
-      this.resourceService.delete(resource.id).subscribe(
-        () => this.resources = this.resources.filter(element => element !== resource),
-        () => alert('Erro ao tentar excluir')
-      );
+    ngOnInit() {
+        this.resourceService.getAll().subscribe(
+            resources => this.resources = resources.sort((a, b) => b.id - a.id),
+            error => alert('Erro ao carregar a lista')
+        );
     }
-  }
+
+    deleteResource(resource: T) {
+        const mustDelete = confirm('Deseja excluir esse item');
+        if (mustDelete) {
+            this.resourceService.delete(resource.id).subscribe(
+                () => this.resources = this.resources.filter(element => element !== resource),
+                () => alert('Erro ao tentar excluir')
+            );
+        }
+    }
 
 }
