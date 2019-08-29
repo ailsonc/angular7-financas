f6f3cec3eb746758a7b8d49a87bb66769000e16c
ailson <ailsonlcosta@gmail.com>
2019-08-29 17:44:46 -0300

    adding ReportsComponent template

--- src/app/pages/reports/reports/reports.component.ts
+++ src/app/pages/reports/reports/reports.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
 
+  monthNames = ['Selecione um mês', 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho',
+      'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+
+  year = new Date().getFullYear();
+  years: Array<any> = [];
+
   constructor() { }
 
   ngOnInit() {
+    for (var i = 0; i < 7; i++) {
+      this.years.push(this.year - i);
+    }
+  }
+
+  public generateReports(){
+    
   }
 
 }
