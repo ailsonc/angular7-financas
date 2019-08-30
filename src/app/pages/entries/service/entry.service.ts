2e58f958a06e9a5eb4bb3927499c1660bf81c111
ailson <ailsonlcosta@gmail.com>
2019-08-30 18:47:06 -0300

    improving reports

--- src/app/pages/entries/service/entry.service.ts
+++ src/app/pages/entries/service/entry.service.ts
@@ -2,6 +2,8 @@ import { Injectable, Injector } from '@angular/core';
 import { BaseResourceService } from '../../../shared/services/base-resource-service';
 import { Entry } from '../models/entry.model';
 import { CategoryService } from '../../categories/service/category.service';
+import { map } from 'rxjs/operators'; 
+import * as moment from "moment";
 
 import { Observable } from 'rxjs';
 import { catchError, flatMap } from 'rxjs/operators';
@@ -21,6 +23,22 @@ export class EntryService extends BaseResourceService<Entry> {
 
   update(entry: Entry): Observable<Entry> {
     return this.setCategoryAndSendToServer(entry, super.update.bind(this));
+  }
+
+  getByMounthAndYear(month: number, year: number): Observable<Entry[]> {
+    // The in-memory database does not filter, it is issued by an API.
+    return this.getAll().pipe(
+      map(entries => this.filterByMonthAndYear(entries,month,year))
+    )
+  }
+
+  private filterByMonthAndYear(entries: Entry[], month: number, year: number): Entry[] {
+    return entries.filter( entry => {
+      const entryData = moment(entry.date, "DD/MM/YYYY");
+      const monthMatches = entryData.month() + 1 == month;
+      const yearMatches = entryData.year() == year;
+      if(monthMatches && yearMatches) return entry;
+    })
   }
 
   private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<Entry> {
